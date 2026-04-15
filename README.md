3742fe2fc44447069e3c0b2923942e96f66a9eca
jupo <jupo@debian.jupowebdev>
2026-04-15 14:42:47 +0200

    express server

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ipssi-socketio-bts
+
+
+## Installer les dépendances :
+
+```
+npm install -D nodemon
+npm install express socket.io
+```
+
+
+Ajout de la commande npm run dev dans package.json
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const fs = require("fs").promises;
+const path = require("path");
+
+const app = express();
+
+app.get("/", async (request, response) => {
+  try {
+    const myPath = path.join(__dirname, "pages/index.html");
+    const html = await fs.readFile(myPath, "utf8");
+    response.send(html);
+  } catch (error) {
+    console.error(error);
+    response.status(500).send("Erreur lors du chargement de la page HTML.");
+  }
+});
+
+
+app.listen(3000, (err) => {
+
+  if (err) {
+      return console.log(err);
+  }
+
+  console.log("Le serveur est lancé sur le port 3000");
+});
